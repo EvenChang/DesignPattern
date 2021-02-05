e5d2b18e72209b93404dd798d8dd6954f08b009f
rabbid15 <rabbid15@hotmail.com>
2021-02-05 13:39:08 +0800

    clear main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
+
     public static void main(String[] args) {
-
-        Map<Integer, Integer> even = new HashMap<>();
-        even.put(1, 2);
-        even.put(2, 3);
-        System.out.println(even);
-        even.compute(2, (key, value) -> {
-            if (key == 2) {
-                return null;
-            }
-
-            return value;
-        });
-        System.out.println(even);
+        
 
     }
 }
